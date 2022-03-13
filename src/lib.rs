78746018687753f4234c7c40084046cc196455aa
Ouvill <contact@ouvill.net>
2022-03-13 18:17:08 +0900

    エンコードの文字指定

--- src/lib.rs
+++ src/lib.rs
@@ -10,38 +10,49 @@ pub use steganography::detect;
 pub use steganography::embed;
 
 enum ZWC {
+    ZeroWidthSpace,
     ZeroWidthNonJoiner,
     ZeroWidthJoiner,
     WordJoiner,
     InvisibleTimes,
+    // 文字方向の指示を元に戻す
+    PopDirectionalFormatting,
+    FunctionApplication,
     InvisibleSeparator,
+
     // ファイルのエンコーディングを示すBOMとしても利用されているので非推奨
-    // ZERO_WIDTH_NON_BREAK,
+    ZERO_WIDTH_NON_BREAK,
     // iOS, IEで表示される
-    // InvisiblePlus
+    InvisiblePlus,
 }
 
 impl ZWC {
     fn value(&self) -> char {
         match *self {
+            ZWC::ZeroWidthSpace => '\u{200B}',
             ZWC::ZeroWidthNonJoiner => '\u{200C}',
             ZWC::ZeroWidthJoiner => '\u{200D}',
+            ZWC::PopDirectionalFormatting => '\u{202C}',
             ZWC::WordJoiner => '\u{2060}',
+            ZWC::FunctionApplication => '\u{2061}',
             ZWC::InvisibleTimes => '\u{2062}',
             ZWC::InvisibleSeparator => '\u{2063}',
-            // ZWC::ZERO_WIDTH_NON_BREAK => '\u{FEFE}',
-            // ZWC::InvisiblePlus => '\u{2064}',
+            ZWC::InvisiblePlus => '\u{2064}',
+            ZWC::ZERO_WIDTH_NON_BREAK => '\u{FEFE}',
         }
     }
 }
 
 fn encode_table() -> [char; 4] {
     [
+        ZWC::ZeroWidthSpace.value(),
         ZWC::ZeroWidthNonJoiner.value(),
         ZWC::ZeroWidthJoiner.value(),
+        // ZWC::PopDirectionalFormatting.value(),
         ZWC::WordJoiner.value(),
-        ZWC::InvisibleTimes.value(),
+        // ZWC::InvisibleTimes.value(),
         // ZWC::InvisibleSeparator.value(),
+        // ZWC::ZERO_WIDTH_NON_BREAK.value(),
     ]
 }
 
